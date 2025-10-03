43f48855864d624e42ad373ba1fdb488ed1c4903
Arun-Saini05 <arunsaini20051223@gmail.com>
2025-10-03 23:11:33 +0530

    Update Task1.py

--- Assignment3/Task1.py
+++ Assignment3/Task1.py
@@ -1,1 +1,9 @@
+def fact(n):
+    if(n==0 or n==1):
+        return 1
+    else:
+        return n*fact(n-1)
 
+n = int(input("Enter the Number:"))
+res = fact(n)
+print(f"The Factorial of {n} is {res}")
